0bc87808877f0c28c6b942a23fca1dcde7768fec
newbalancem5 <newbalancem5@gmail.com>
2020-06-02 13:02:40 +0300

    upd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,19 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "c66e2f6d2d5e2bfb79516c9290f8d20c",
+"assets/assets/images/icon.png": "0acc4fb27dca6764227bcc59d514d678",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "7f1df1ee47854388e684afc0845ea820",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "0acc4fb27dca6764227bcc59d514d678",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/icon.png": "0acc4fb27dca6764227bcc59d514d678",
 "index.html": "515e025380bd8651e763aef49e3cb911",
 "/": "515e025380bd8651e763aef49e3cb911",
-"main.dart.js": "c1f4a4ed08629632e0657a069501421a",
+"main.dart.js": "09c04da3cc31577a06de0c7f88e9c9b8",
 "manifest.json": "db6fa91835b95bbbc4f1d2c9714b8f5e"
 };
 
